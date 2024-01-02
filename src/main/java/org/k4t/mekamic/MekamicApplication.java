4729736c99c34a422958867dcd5603414a181f3a
K4T <tuanka197st@gmail.com>
2024-01-03 00:10:40 +0700

    init project

---
+++ src/main/java/org/k4t/mekamic/MekamicApplication.java
@@ -0,0 +1,13 @@
+package org.k4t.mekamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MekamicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MekamicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/k4t/mekamic/MekamicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.k4t.mekamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MekamicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
